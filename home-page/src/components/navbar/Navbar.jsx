33a2cd0743f4af9d0573d3d4a6a5bc23e65a89b2
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-12-06 17:46:57 +0530

    done with tablet screening part

--- home-page/src/components/navbar/Navbar.jsx
+++ home-page/src/components/navbar/Navbar.jsx
@@ -3,43 +3,79 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar border-body" data-bs-theme="dark">
-      <div className="logo">
-        <img src="./assets/logo.png" alt="" />
-      </div>
-      <div className="right d-flex align-items-center ">
-        <ul className="d-flex ">
-          <li>
-            <a href="#">Home</a>
-          </li>
-
-          <li className="nav-item dropdown">
-            <a href="#" className="nav-link dropdown-toggle">
-              Our Story
-            </a>
-          </li>
-          <li>
-            <a href="#">Success & ABILITY</a>
-          </li>
-
-          <li>
-            <a href="#">Media</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a href="#" className="nav-link dropdown-toggle">
-              Employment
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a href="#" className="nav-link dropdown-toggle">
-              Events
-            </a>
-          </li>
-        </ul>
-        <div>
-          <button type="button" class="btn">
-            Let's Connect
-          </button>
+    <nav
+      className="navbar navbar-expand-lg navbar-light border-body"
+      data-bs-theme="dark"
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand logo" href="#">
+          <img src="./assets/logo.png" alt="Logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="offcanvas offcanvas-end"
+          tabIndex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              Menu
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button">
+                  Our Story
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Success & ABILITY
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Media
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button">
+                  Employment
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button">
+                  Events
+                </a>
+              </li>
+            </ul>
+            <div className="text-center">
+              <button type="button" className="btn btn-primary">
+                Let's Connect
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
